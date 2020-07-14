8c13c68567c5226334f231661587d71201463693
PhilCAEUser <mail@rwth>
2020-07-14 10:20:03 +0000

    Http Response[resp1] edited by Philipp Dolif

--- src/main/i5/las2peer/services/res2/res2.java
+++ src/main/i5/las2peer/services/res2/res2.java
@@ -127,7 +127,7 @@ public class res2 extends RESTService {
     boolean resp1_condition = true;
     if(resp1_condition) {
       JSONObject result1 = new JSONObject();
-
+      result1.set("test1", "")
       
 
       return Response.status(HttpURLConnection.HTTP_OK).entity(result1.toJSONString()).build();
